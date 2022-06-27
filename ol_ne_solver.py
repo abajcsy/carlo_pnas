5a4407108dd0132d0c656a2b716236324698f3fc
james.jingqi.li <52582752+jamesjingqili@users.noreply.github.com>
2022-06-27 09:43:10 -0700

    Added the OLNE solver for quadratic-affine costs

--- ol_ne_solver.py
+++ ol_ne_solver.py
@@ -36,3 +36,67 @@ def OLNE_2player_solver(A,B1,B2,Q1,Q2,R1,R2,T,x0):
                 + 1/2*u2_traj[:,t].T@R2@u2_traj[:,t]
 
     return x_traj, u1_traj, u2_traj, J1, J2
+
+
+def OLNE_quadratic_affine_cost_2player_solver(A,B1,B2,Q1,Q2,R1,R2,T,x0,q1,q2):
+    # consider the cost: 1/2 x'Qx + q'x + 1/2 u'Ru
+    
+    n = len(x0)
+    m1 = B1.shape[1]
+    m2 = B2.shape[1]
+    Q1_block = np.kron(np.eye(T), Q1)
+    Q2_block = np.kron(np.eye(T), Q2)
+    R1_block = np.kron(np.eye(T), R1)
+    R2_block = np.kron(np.eye(T), R2)
+    B1_block = np.kron(np.eye(T), B1)
+    B2_block = np.kron(np.eye(T), B2)
+    upper_triangle_matrix = np.zeros((T,T))
+    upper_triangle_matrix[:T-1,1:] = np.eye(T-1)
+    
+    lower_triangle_matrix = np.zeros((T,T))
+    lower_triangle_matrix[1:,:T-1] = np.eye(T-1)
+
+    A_upper_block = np.kron(upper_triangle_matrix, -A.T) + np.eye(n*T)
+    A_lower_block = np.kron(lower_triangle_matrix, -A) + np.eye(n*T)
+
+    M = np.zeros(((3*n+m1+m2)*T, (3*n+m1+m2)*T))
+    # The first layer block:
+    M[:n*T, :n*T] = Q1_block
+    M[:n*T, (n+m1+m2)*T:(2*n+m1+m2)*T] = A_upper_block
+    # The second layer block:
+    M[n*T:(n+m1)*T, n*T:(n+m1)*T] = R1_block
+    M[n*T:(n+m1)*T, (n+m1+m2)*T:(2*n+m1+m2)*T] = -B1_block.T
+    # The third layer block:
+    M[(n+m1)*T:(2*n+m1)*T, :n*T] = Q2_block
+    M[(n+m1)*T:(2*n+m1)*T, (2*n+m1+m2)*T:] = A_upper_block
+    # The fourth layer block:
+    M[(2*n+m1)*T:(2*n+m1+m2)*T, (n+m1)*T:(n+m1+m2)*T] = R2_block
+    M[(2*n+m1)*T:(2*n+m1+m2)*T, (2*n+m1+m2)*T:] = - B2_block.T
+    # The fifth layer block:
+    M[(2*n+m1+m2)*T:, :n*T] = A_lower_block
+    M[(2*n+m1+m2)*T:, n*T:(n+m1)*T] = -B1_block
+    M[(2*n+m1+m2)*T:, (n+m1)*T:(n+m1+m2)*T] = -B2_block
+
+
+    N = np.zeros(((3*n+m1+m2)*T, 1))
+    N[:n*T] = np.kron(np.ones((T,1)), -q1)
+    N[(n+m1)*T:(n+m1+n)*T] = np.kron(np.ones((T,1)), -q2)
+    N[(2*n+m1+m2)*T:(2*n+m1+m2)*T+n] = A @ x0
+
+
+    sol = np.linalg.inv(M) @ N
+
+    x_traj = np.concatenate((x0.reshape(-1,1),  sol[:n*T].reshape(T,n).T ),1)
+    u1_traj = sol[n*T:(n+m1)*T].reshape(T,m1).T
+    u2_traj = sol[(n+m1)*T:(n+m1+m2)*T].reshape(T,m2).T
+    J1 = 1/2*x0.T@Q1@x0 + q1.T@x0
+    J2 = 1/2*x0.T@Q2@x0 + q2.T@x0
+
+    for t in range(T):
+        J1 = J1 + 1/2*x_traj[:,t+1].T@Q1@x_traj[:,t+1] \
+                + 1/2*u1_traj[:,t].T@R1@u1_traj[:,t] \
+                + q1.T @ x_traj[:,t+1]
+        J2 = J2 + 1/2*x_traj[:,t+1].T@Q2@x_traj[:,t+1] \
+                + 1/2*u2_traj[:,t].T@R2@u2_traj[:,t] \
+                + q2.T @ x_traj[:,t+1]
+    return x_traj, u1_traj, u2_traj, J1, J2
